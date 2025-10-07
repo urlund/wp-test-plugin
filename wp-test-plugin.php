b4ce3f5e524adf8e4e69fe96bc80741cddacd81e
Henrik Urlund <henrik@urlund.com>
2025-10-07 16:55:47 +0200

    Bump version to 1.0.2

--- wp-test-plugin.php
+++ wp-test-plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name: WP Test Plugin
  * Plugin URI: https://github.com/urlund/wp-test-plugin
  * Description: A simple WordPress plugin boilerplate for testing and development
- * Version: 1.0.1
+ * Version: 1.0.2
  * Author: Henrik Urlund
  * Author URI: https://github.com/urlund
  * License: MIT
